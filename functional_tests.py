658ab328a3c6d35ff706c3891ff5d4afd52fbc25
Maritza <maritzaesparza05@gmail.com>
2016-06-27 17:56:41 -0500

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -18,7 +18,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.broser.find_element_by_tag_name('h1').text
+        header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
 
@@ -40,13 +40,14 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feather to make a fly" (Edith is very methodical)
         self.fail('Finish the test!')
-        
+
         # The page updates again, and now shows both items on her list
 
         # Edith wonders whether the site will remmember her list. Then she sees
